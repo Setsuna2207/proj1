a6cf2cf5a1d7a20622c98e6fd543be9c14abb4be
Setsuna2207 <setsuna2207.yae@gmail.com>
2024-10-17 19:27:43 +0700

    vid1

---
+++ vid1/bai1.js
@@ -0,0 +1,14 @@
+var name = 'Học React, JavaScript';
+var date = '17 tháng 10 năm 2024';
+
+
+const x = {};
+const y = {};
+
+x.name = 'Học React, JavaScript';
+y.date = 'var = cook; let = smaller but better var ; const = vua';
+
+console.log(">>> ktra x: ", x);
+console.log(">>> Bài 1: ", y);
+
+//Bài 1: var = cook; let = smaller but better var ; const = vua

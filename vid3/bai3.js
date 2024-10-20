29ef63074adc88064ccc97aaf37b50b7b29fec8d
Setsuna2207 <setsuna2207.yae@gmail.com>
2024-10-20 21:53:56 +0700

    vid3

---
+++ vid3/bai3.js
@@ -0,0 +1,25 @@
+// const person = {name: "Tuýn", age: "20", prop: "Nghèo"};
+
+//Object: {name1: "value1", name2: "value2"}
+
+// console.log(">>> Ktra: ", person)
+
+// console.log(">>> ktra tên: ", person.name)
+
+// console.log(">>> ktra tuổi: ", person["age"])
+
+//---------------//
+
+// const person = ["Tuýn", "20", "Đói"];
+
+// console.log(">>> ktra tên: ", person[0])
+
+//---------------//
+
+let tutorial = {
+    name: "React Tutorial",
+    author: "NHBT",
+    language: "JavaScript"
+};
+
+alert(tutorial.name)

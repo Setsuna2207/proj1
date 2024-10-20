afd4f10d88560b5b2cd4253682fd788ae41cadbc
Setsuna2207 <setsuna2207.yae@gmail.com>
2024-10-20 22:14:57 +0700

    vid4

---
+++ vid4/bai4.js
@@ -0,0 +1,12 @@
+// function test (){
+//     console.log(">>> test: ")
+// }
+// test();
+
+test = () => console.log(">>> test: ")
+
+test();
+
+hello = (value1, value2) => "Hello world, " + value1 + value2;
+
+console.log(hello("Tuýn. ", "Have a nice day!"))
